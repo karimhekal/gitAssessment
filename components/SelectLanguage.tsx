4a93b918abf342a90d8eef76ce0003d29a2d0171
Karim Hekal <karimhekal@gmail.com>
2023-09-11 12:50:42 +0300

    fixes

--- components/SelectLanguage.tsx
+++ components/SelectLanguage.tsx
@@ -1,17 +1,14 @@
 import React, {useState} from 'react';
 import {
-  Button,
   Dimensions,
   FlatList,
   ScrollView,
   StyleSheet,
-  TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
 import {Text} from './Text';
 import {Theme} from '../Config';
-import {Icon} from 'react-native-vector-icons/Icon';
 import {FormField} from './FormField';
 export default function SelectLanguage({
   setModalVisible,
@@ -42,7 +39,6 @@ export default function SelectLanguage({
     <View style={styles.container}>
       <View style={styles.header}>
         <Text>Select Language</Text>
-
         <TouchableOpacity
           style={styles.x}
           onPress={() => {
@@ -56,8 +52,7 @@ export default function SelectLanguage({
         clearButton
         style={styles.input}
       />
-      <ScrollView
-        style={{width: '100%', height: Dimensions.get('screen').height / 2}}>
+      <ScrollView contentContainerStyle={styles.scroll}>
         <FlatList
           showsVerticalScrollIndicator
           renderItem={({item}) => {
@@ -77,6 +72,7 @@ export default function SelectLanguage({
   );
 }
 const styles = StyleSheet.create({
+  scroll: {width: '100%', height: Dimensions.get('screen').height / 2},
   container: {
     backgroundColor: 'white',
     padding: Theme.spacing.m,
@@ -101,9 +97,10 @@ const styles = StyleSheet.create({
     fontSize: 12,
     lineHeight: 16,
     color: Theme.colors.background,
+    fontFamily: Theme.fonts.EnglishBold,
   },
   x: {
-    backgroundColor: Theme.colors.secondary,
+    backgroundColor: 'rgba(204, 212, 221, 1)',
     borderRadius: 10000,
     display: 'flex',
     justifyContent: 'center',
